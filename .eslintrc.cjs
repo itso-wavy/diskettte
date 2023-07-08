5f10674ba20ae22679c8bd7e8418038994d722e9
itso-wavy <you.meet.vvavy@gmail.com>
2023-07-08 18:00:03 +0900

    docs: 개발 환경 세팅

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+/* eslint-env node */
+
+module.exports = {
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+	],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: { react: { version: '18.2' } },
+	plugins: ['react', 'react-refresh'],
+	rules: {
+		'no-unused-vars': [
+			'error',
+			{
+				varsIgnorePattern: 'React',
+			},
+		],
+		'react/jsx-uses-react': 'error',
+		'react/jsx-uses-vars': 'error',
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+	},
+};
---
+++ README.md
@@ -0,0 +1,20 @@
+# diskette
+
+- 자체 컨벤션
+- 컴포넌트 세분화
+
+## 유저 로그인 시나리오
+
+<!--
+
+diskette
+glazed
+naive
+obj
+kkiosk
+ours
+cassette
+tape
+floppy
+apArt
+bake-pack -->
