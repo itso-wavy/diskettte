7472aa93efb8619f7abec3311e55e4f87c7e5973
itso-wavy <you.meet.vvavy@gmail.com>
2023-07-24 04:25:16 +0900

    feat: Modal 내부 클릭시 닫히지 않도록 조정

--- src/components/@ui/Modal.jsx
+++ src/components/@ui/Modal.jsx
@@ -1,10 +1,19 @@
+import { useRef } from 'react'
 import { createPortal } from 'react-dom'
 import { Backdrop, ModalOverlay } from './Modal.style'
 
 export default function Modal({ closeModal, children, ...props }) {
+	const modalRef = useRef()
+
+	const onClickHandler = e => {
+		e.target !== modalRef.current && closeModal()
+	}
+
 	const modalLayout = (
-		<Backdrop onClick={closeModal}>
-			<ModalOverlay {...props}>{children}</ModalOverlay>
+		<Backdrop onClick={onClickHandler}>
+			<ModalOverlay ref={modalRef} {...props}>
+				{children}
+			</ModalOverlay>
 		</Backdrop>
 	)
 
