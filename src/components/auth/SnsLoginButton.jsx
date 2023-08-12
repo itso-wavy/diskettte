42af7840bad39064471c6702b8906a4cfb5c3c78
itso-wavy <you.meet.vvavy@gmail.com>
2023-08-13 00:41:30 +0900

    feat: 날아간 파일 임시 복구...

--- src/components/auth/SnsLoginButton.jsx
+++ src/components/auth/SnsLoginButton.jsx
@@ -1,5 +1,8 @@
 import { Button } from '../@ui/Button'
 import { Img } from '../@ui/Img'
+import googleImg from '/public/assets/icons/flat-color-icons_google.svg'
+import kakaotalkImg from '/public/assets/icons/simple-icons_kakaotalk.svg'
+import naverImg from '/public/assets/icons/simple-icons_naver.svg'
 
 function SnsLoginButton({ src, text, ...props }) {
 	return (
@@ -10,4 +13,19 @@ function SnsLoginButton({ src, text, ...props }) {
 	)
 }
 
-export { SnsLoginButton }
+function GoogleLoginButton() {
+	return <SnsLoginButton src={googleImg} text='구글 계정으로 로그인' />
+}
+function KakaotalkLoginButton() {
+	return <SnsLoginButton src={kakaotalkImg} text='카카오톡 계정으로 로그인' />
+}
+function NaverLoginButton() {
+	return <SnsLoginButton src={naverImg} text='네이버 계정으로 로그인' />
+}
+
+export {
+	SnsLoginButton,
+	GoogleLoginButton,
+	KakaotalkLoginButton,
+	NaverLoginButton,
+}
