49a528e40a1fd2607d5c4fd2230b0c04b962b200
itso-wavy <you.meet.vvavy@gmail.com>
2023-09-05 13:18:03 +0900

    feat: 1시간 동안 페이지 이동 없을시 자동 로그아웃

--- src/components/@layout/RootLayout.jsx
+++ src/components/@layout/RootLayout.jsx
@@ -1,19 +1,32 @@
 import { useEffect } from 'react'
-import { Outlet, useLocation, useNavigation } from 'react-router-dom'
+import {
+	Outlet,
+	useLocation,
+	useNavigate,
+	useNavigation,
+} from 'react-router-dom'
 import { ScrollToTop, Loading } from '../common'
 import { Header } from '../header'
 import { Footer } from '../footer'
 // import { Popup } from '../@ui/Popup'
 import { StyledMain } from './RootLayout.style'
-// import useStore from '../../store'
+import useStore from '../../store'
 
 export function RootLayout() {
+	const navigate = useNavigate()
 	const { state } = useNavigation()
 	const { pathname, search } = useLocation()
+	const { isSignedIn } = useStore()
 	// const { isPoppedUp } = useStore()
 
 	useEffect(() => {
 		window.scrollTo(0, 0)
+
+		let timer
+		if (isSignedIn)
+			timer = setTimeout(() => navigate('/auth/logout'), 60 * 60 * 1000)
+
+		return () => clearTimeout(timer)
 	}, [pathname, search])
 
 	return (
